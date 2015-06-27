93ae380f6da3d3c441b6f50298e6e83b380774d2
Ani <ani.stoicheva@gmail.com>
2015-06-27 15:03:47 +0300

    Adding solutions for Roots and Birthday Ranges.

--- week2/2-Birthday-Ranges/birthday_ranges.py
+++ week2/2-Birthday-Ranges/birthday_ranges.py
@@ -4,4 +4,24 @@ class BirthdayRanges:
     # birthdays - [int]
     # ranges - [(int, int)]
     def birthdays_count(birthdays, ranges):
-        pass
+        dates = [0] * 366
+        prefix_sum = [0]
+        result = []
+
+        for date in birthdays:
+            dates[date] += 1
+
+        for date_count in dates:
+            prefix_sum.append(prefix_sum[-1] + date_count)
+
+        for element in ranges:
+            result.append(prefix_sum[element[-1] + 1] - prefix_sum[element[0]])
+
+        return result
+
+
+birthdays = [5, 10, 6, 7, 3, 4, 5, 11, 21, 300, 15]
+ranges = [(4, 9), (6, 7), (200, 225), (300, 365)]
+
+a = BirthdayRanges.birthdays_count(birthdays, ranges)
+print(a)
